984d85b96876ec9f0a958045025453c672e8dc3e
yzhu <891085309@qq.com>
2022-03-13 02:46:09 +0800

    [feat] Improve compatibility: platform, cwd

--- javascript/polyfill.js
+++ javascript/polyfill.js
@@ -1,6 +1,15 @@
 /* global globalThis */
 import * as os from 'os'
-globalThis.process = { env: { NODE_ENV: 'development' } }
+globalThis.process = { 
+    env: { NODE_ENV: 'development' },
+    platform: os.platform,
+    cwd: () =>{
+        const [path, err]  = os.getcwd()
+        if(err !== 0 )
+            throw new Error(" os.cwd() errno")
+        return path
+    }
+ }
 if (!globalThis.setTimeout) globalThis.setTimeout = os.setTimeout
 globalThis.console.warn = console.log
 globalThis.console.error = () => {}
