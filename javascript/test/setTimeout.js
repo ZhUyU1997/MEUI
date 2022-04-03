fd69cc6638e50368359b7efb97e775aa55668973
yzhu <891085309@qq.com>
2022-04-04 04:39:28 +0800

    [test] setTimeout

---
+++ javascript/test/setTimeout.js
@@ -0,0 +1,25 @@
+import * as std from "std"
+import * as os from "os"
+
+for (let i = 0; i < 10000; i++) {
+    const timer = os.setTimeout(() => {}, 100)
+}
+
+
+for (let i = 0; i < 10000; i++) {
+    const timer = os.setTimeout(() => {}, 100)
+}
+
+function test() {
+    let timer = null
+    function run() {
+        timer = os.setTimeout(() => {
+            console.log(run)
+        }, 1000)
+    }
+    run()
+}
+
+test();
+
+std.gc();
