80fddaa05cdddbf583da8c66f2bb373cb29696f8
yzhu <891085309@qq.com>
2022-04-04 04:40:09 +0800

    [config] support typescript

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import { babel } from '@rollup/plugin-babel'
+import typescript from 'rollup-plugin-typescript2';
 
 export default {
 	input: 'javascript/index.jsx',
@@ -20,5 +21,6 @@ export default {
 			// presets: ['@babel/preset-react'],
 			babelHelpers: 'bundled', exclude: ['node_modules/**']
 		}),
+		typescript(),
 	]
 }
