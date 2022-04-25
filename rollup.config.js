8b3982c52613caddb149b6720f1e1fd88cd08e48
yzhu <891085309@qq.com>
2022-04-25 21:12:25 +0800

    🦄 refactor: javascript => framework

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import { babel } from '@rollup/plugin-babel'
 import typescript from 'rollup-plugin-typescript2'
 
 export default {
-	input: 'javascript/index.jsx',
+	input: 'framework/index.jsx',
 	output: {
 		format: 'esm',
 		dir: "dist",
