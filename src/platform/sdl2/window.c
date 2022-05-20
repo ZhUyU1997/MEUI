1a416e3cb4481146ffa8f46e7bc426ee3e419c17
vsfos <vsfos@foxmail.com>
2022-05-20 12:35:18 +0800

    feature: convert key code to key string in vsf/sdl platform

--- src/platform/sdl2/window.c
+++ src/platform/sdl2/window.c
@@ -113,6 +113,150 @@ int window_pending(struct window_t *window)
     return (int)u;
 }
 
+// covert HID scancode to w3c key value
+// https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values
+static const char * vsf_input_keyboard_get_key(struct window_t *window, SDL_Event *evt)
+{
+    static const char * keymap[] = {
+        NULL, NULL, NULL, NULL,     // 0 - 3
+
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6",
+        "7", "8", "9", "0",         // 4 - 39
+        "Enter",                    // 40
+        "Escape",                   // 41
+        "Backspace",                // 42
+        "Tab",                      // 43
+        " ", "-", "=", "[", "]", "\\", "#", ";", "'", ",", ".", "/",
+                                    // 44 - 56
+        "CapsLock",                 // 57
+        "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12",
+                                    // 58 - 69
+        "PrintScreen",              // 70
+        "ScrollLock",               // 71
+        "Pause",                    // 72
+        "Insert",                   // 73
+        "Home",                     // 74
+        "PageUp",                   // 75
+        "Delete",                   // 76
+        "End",                      // 77
+        "PageDown",                 // 78
+        "ArrowRight",               // 79
+        "ArrowLeft",                // 80
+        "ArrowDown",                // 81
+        "ArrowUp",                  // 82
+        "NumLock",                  // 83
+        "Divide",                   // 84
+        "Multiply",                 // 85
+        "Subtract",                 // 86
+        "Add",                      // 87
+        "Enter",                    // 88
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+                                    // 89 - 98
+        "Decimal",                  // 99
+        NULL,                       // 100
+        "Compose",                  // 101
+        "Power",                    // 102
+        "=",                        // 103
+        "F13", "F14", "F15",        // 104 - 106
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                    // 107 - 115: F16 - F24
+        "Execute",                  // 116
+        "Help",                     // 117
+        "Props",                    // 118
+        "Select",                   // 119
+        "Stop",                     // 120 TODO:
+        "Again",                    // 121
+        "Undo",                     // 122
+        "Cut",                      // 123
+        "Copy",                     // 124
+        "Paste",                    // 125
+        "Find",                     // 126
+        "AudioVolumeMute",          // 127
+        "AudioVolumeUp",            // 128
+        "AudioVolumeDown",          // 129
+        NULL, NULL, NULL,           // 130 - 132: Locking CapsLock, Locking NumLock, Locking ScrollLock
+        "Separator",                // 133
+        NULL,                       // 134
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                    // 135 - 143: International1 - International9
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                    // 144 - 152: LANG1 - LANG9
+        NULL,                       // 153: Alternate Erase
+        "Attn",                     // 154
+        "Cancel",                   // 155
+        "Clear",                    // 156
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        "CrSel",                    // 163
+        "ExSel",                    // 164
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL,                 // 176 - 177: KP_00 - KP_000
+        NULL, NULL,                 // 178 - 179: Thounds Separator - Decimal Separator
+        NULL, NULL,                 // 180 - 181: Currency Unit - Currency Sub-unit
+
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, 
+//        "(", ")", "{", "}", "Tab", "Backspace", "A", "B", "C", "D", "E", "F",
+//        "XOR", "^", "%", "<", ">", "&", "&&", "|", "||", ":", "#", " ", "@", "!",
+                                    // 182 - 207
+
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                    // 208 - 214: Memory Store/Recall/Clear/Add/Subtract/Multiply/Divide
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                    // 215 - 221: Keypad +-/Clear/Clear Entry/Binary/Octal/Decimal/Hexadecimal
+        NULL, NULL,
+        "Control", "Shift", "Alt", "Meta",
+        "Control", "Shift", "Alt", "Meta",
+                                    // 224 - 231
+        NULL,                       // 232: MEDIA PLAYPAUSE
+        NULL,                       // 233: MEDIA STOPCD
+        "MediaTrackPrevious",       // 234
+        "MediaTrackNext",           // 235
+        "Eject",                    // 236
+        "AudioVolumeUp",            // 237
+        "AudioVolumeDown",          // 238
+        "AudioVolumeMute",          // 239
+        "LaunchWebBrowser",         // 240
+        NULL, NULL,                 // 241 - 242: MEDIA BACK/FORWARD
+        "MediaStop",                // 243
+        NULL, NULL, NULL,           // 244: MEDIA FIND/SCROLLUP/SCROLLDOWN
+        NULL,                       // 245: MEDIA EDIT
+        NULL,                       // 246: MEDIA SLEEP
+        NULL,                       // 247: MEDIA COFFEE
+        NULL,                       // 248: MEDIA REFRESH
+        NULL,                       // 249: MEDIA CALC
+    };
+    static const char * KEYMAP[] = {
+        NULL, NULL, NULL, NULL,     // 0 - 3
+
+        "A", "B", "C", "D", "E", "F", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+        "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "@", "#", "$", "%", "^",
+        "&", "*", "(", ")",         // 4 - 39
+        NULL, NULL, NULL, NULL,     // 40 - 43
+        " ", "_", "+", "{", "}", "|", "#", ":", "\"", "<", ">", "?",
+                                    // 44 - 56
+    };
+
+    Uint16 mod = evt->key.keysym.mod;
+    SDL_Scancode keycode = evt->key.keysym.scancode;
+    bool is_upper = false;
+
+    if (    (mod & KMOD_CAPS)
+        &&  ((keycode >= SDL_SCANCODE_A) && (keycode <= SDL_SCANCODE_Z))) {
+        is_upper = !is_upper;
+     }
+    if (    (mod & KMOD_SHIFT)
+        &&  (   ((keycode >= SDL_SCANCODE_1) && (keycode <= SDL_SCANCODE_0))
+            ||  ((keycode >= SDL_SCANCODE_MINUS) && (keycode <= SDL_SCANCODE_SLASH))
+            )
+       ) {
+        is_upper = !is_upper;
+    }
+    const char *str = is_upper ? KEYMAP[keycode] : keymap[keycode];
+    return (keycode > dimof(keymap)) || (NULL == str) ? "Unidentified" : str;
+}
+
 void window_next_event(struct window_t *window, struct meui_event_t *meui_event)
 {
     SDL_Event event;
@@ -130,7 +274,7 @@ read_next:
             .type           = event.type == SDL_KEYDOWN ? MEUI_EVENT_KEY_DOWN : MEUI_EVENT_KEY_UP,
             .KEY_DOWN       = {
                 .keyCode    = event.key.keysym.scancode,
-                .key        = SDL_GetKeyName(event.key.keysym.sym),
+                .key        = vsf_input_keyboard_get_key(window, &event),
                 .shiftKey   = event.key.keysym.mod & KMOD_SHIFT,
                 .ctrlKey    = event.key.keysym.mod & KMOD_CTRL,
                 .altKey     = event.key.keysym.mod & KMOD_ALT,
--- src/platform/vsf/window.c
+++ src/platform/vsf/window.c
@@ -43,6 +43,7 @@ struct window_t {
         struct {
             vsf_linux_fd_t *sfd;
             vk_input_notifier_t notifier;
+            bool capslock;
         } event;
         struct {
             vsf_linux_fd_t *sfd;
@@ -156,6 +157,150 @@ int window_pending(struct window_t *window)
     return vsf_stream_get_data_size(stream) / sizeof(window_evt_t);
 }
 
+// covert HID scancode to w3c key value
+// https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values
+static const char * vsf_input_keyboard_get_key(struct window_t *window, vk_input_evt_t *evt)
+{
+    static const char * keymap[] = {
+        NULL, NULL, NULL, NULL,     // 0 - 3
+
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6",
+        "7", "8", "9", "0",         // 4 - 39
+        "Enter",                    // 40
+        "Escape",                   // 41
+        "Backspace",                // 42
+        "Tab",                      // 43
+        " ", "-", "=", "[", "]", "\\", "#", ";", "'", ",", ".", "/",
+                                    // 44 - 56
+        "CapsLock",                 // 57
+        "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12",
+                                    // 58 - 69
+        "PrintScreen",              // 70
+        "ScrollLock",               // 71
+        "Pause",                    // 72
+        "Insert",                   // 73
+        "Home",                     // 74
+        "PageUp",                   // 75
+        "Delete",                   // 76
+        "End",                      // 77
+        "PageDown",                 // 78
+        "ArrowRight",               // 79
+        "ArrowLeft",                // 80
+        "ArrowDown",                // 81
+        "ArrowUp",                  // 82
+        "NumLock",                  // 83
+        "Divide",                   // 84
+        "Multiply",                 // 85
+        "Subtract",                 // 86
+        "Add",                      // 87
+        "Enter",                    // 88
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+                                    // 89 - 98
+        "Decimal",                  // 99
+        NULL,                       // 100
+        "Compose",                  // 101
+        "Power",                    // 102
+        "=",                        // 103
+        "F13", "F14", "F15",        // 104 - 106
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                    // 107 - 115: F16 - F24
+        "Execute",                  // 116
+        "Help",                     // 117
+        "Props",                    // 118
+        "Select",                   // 119
+        "Stop",                     // 120 TODO:
+        "Again",                    // 121
+        "Undo",                     // 122
+        "Cut",                      // 123
+        "Copy",                     // 124
+        "Paste",                    // 125
+        "Find",                     // 126
+        "AudioVolumeMute",          // 127
+        "AudioVolumeUp",            // 128
+        "AudioVolumeDown",          // 129
+        NULL, NULL, NULL,           // 130 - 132: Locking CapsLock, Locking NumLock, Locking ScrollLock
+        "Separator",                // 133
+        NULL,                       // 134
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                    // 135 - 143: International1 - International9
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                    // 144 - 152: LANG1 - LANG9
+        NULL,                       // 153: Alternate Erase
+        "Attn",                     // 154
+        "Cancel",                   // 155
+        "Clear",                    // 156
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        "CrSel",                    // 163
+        "ExSel",                    // 164
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL,                 // 176 - 177: KP_00 - KP_000
+        NULL, NULL,                 // 178 - 179: Thounds Separator - Decimal Separator
+        NULL, NULL,                 // 180 - 181: Currency Unit - Currency Sub-unit
+
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+        NULL, NULL, 
+//        "(", ")", "{", "}", "Tab", "Backspace", "A", "B", "C", "D", "E", "F",
+//        "XOR", "^", "%", "<", ">", "&", "&&", "|", "||", ":", "#", " ", "@", "!",
+                                    // 182 - 207
+
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                    // 208 - 214: Memory Store/Recall/Clear/Add/Subtract/Multiply/Divide
+        NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                                    // 215 - 221: Keypad +-/Clear/Clear Entry/Binary/Octal/Decimal/Hexadecimal
+        NULL, NULL,
+        "Control", "Shift", "Alt", "Meta",
+        "Control", "Shift", "Alt", "Meta",
+                                    // 224 - 231
+        NULL,                       // 232: MEDIA PLAYPAUSE
+        NULL,                       // 233: MEDIA STOPCD
+        "MediaTrackPrevious",       // 234
+        "MediaTrackNext",           // 235
+        "Eject",                    // 236
+        "AudioVolumeUp",            // 237
+        "AudioVolumeDown",          // 238
+        "AudioVolumeMute",          // 239
+        "LaunchWebBrowser",         // 240
+        NULL, NULL,                 // 241 - 242: MEDIA BACK/FORWARD
+        "MediaStop",                // 243
+        NULL, NULL, NULL,           // 244: MEDIA FIND/SCROLLUP/SCROLLDOWN
+        NULL,                       // 245: MEDIA EDIT
+        NULL,                       // 246: MEDIA SLEEP
+        NULL,                       // 247: MEDIA COFFEE
+        NULL,                       // 248: MEDIA REFRESH
+        NULL,                       // 249: MEDIA CALC
+    };
+    static const char * KEYMAP[] = {
+        NULL, NULL, NULL, NULL,     // 0 - 3
+
+        "A", "B", "C", "D", "E", "F", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+        "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "@", "#", "$", "%", "^",
+        "&", "*", "(", ")",         // 4 - 39
+        NULL, NULL, NULL, NULL,     // 40 - 43
+        " ", "_", "+", "{", "}", "|", "#", ":", "\"", "<", ">", "?",
+                                    // 44 - 56
+    };
+
+    uint8_t mod = vsf_input_keyboard_get_keymod(evt);
+    uint16_t keycode = vsf_input_keyboard_get_keycode(evt);
+    bool is_shift_down = mod & VSF_KM_SHIFT, is_upper = false;
+
+    if (    window->event.capslock
+        &&  ((keycode >= VSF_KB_a) && (keycode <= VSF_KB_z))) {
+        is_upper = !is_upper;
+     }
+    if (    is_shift_down
+        &&  (   ((keycode >= VSF_KB_1) && (keycode <= VSF_KB_0))
+            ||  ((keycode >= VSF_KB_MINUS) && (keycode <= VSF_KB_SLASH))
+            )
+       ) {
+        is_upper = !is_upper;
+    }
+    const char *str = is_upper ? KEYMAP[keycode] : keymap[keycode];
+    return (keycode > dimof(keymap)) || (NULL == str) ? "Unidentified" : str;
+}
+
 void window_next_event(struct window_t *window, struct meui_event_t *meui_event)
 {
     window_evt_t evt;
@@ -174,12 +319,15 @@ read_next:
                 .type           = vsf_input_keyboard_is_down(&evt.evt) ? MEUI_EVENT_KEY_DOWN : MEUI_EVENT_KEY_UP,
                 .KEY_DOWN       = {
                     .keyCode    = vsf_input_keyboard_get_keycode(&evt.evt),
-                    .key        = NULL,     // TODO
+                    .key        = vsf_input_keyboard_get_key(window, &evt.evt),
                     .shiftKey   = mod & VSF_KM_SHIFT,
                     .ctrlKey    = mod & VSF_KM_CTRL,
                     .altKey     = mod & VSF_KM_ALT,
                 },
             };
+            if ((MEUI_EVENT_KEY_DOWN == meui_event->type) && (VSF_KB_CAPSLOCK == meui_event->KEY_DOWN.keyCode)) {
+                window->event.capslock = !window->event.capslock;
+            }
         }
         break;
     case VSF_INPUT_TYPE_MOUSE:
