acc298961936c9e6d80c283ae885accef20ef12d
yzhu <891085309@qq.com>
2022-10-15 23:42:17 +0800

    ✨ feat: add watch/timer

---
+++ examples/watch/timer.jsx
@@ -0,0 +1,97 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Yu Zhu <891085309@qq.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+import React, { useEffect, useState } from "react"
+import { Div, MEUI, Stack } from "@/meui"
+import ReactMEUI from "@/react-meui"
+import { Root } from "@/components"
+
+function App() {
+    const [time, setTime] = useState(0)
+    const [status, setStatus] = useState("init")
+
+    useEffect(() => {
+        if (status === "running") {
+            const id = setInterval(() => {
+                setTime((prev) => prev + 10)
+            }, 10)
+
+            return () => {
+                clearInterval(id)
+            }
+        }
+    }, [status])
+    return (
+        <Root>
+            <Div
+                style={{
+                    backgroundColor: "black",
+                    borderRadius: 40,
+                    width: "100%",
+                    flexDirection: "column",
+                    padding: 20,
+                }}
+            >
+                <Div
+                    style={{
+                        fontColor: "white",
+                        fontSize: 100,
+                        textAlign: "center",
+                        flexGrow: 1,
+                    }}
+                >
+                    {(time / 1000).toFixed(0).padStart(2, "0")}:
+                    {(time % 1000).toString().padStart(3, "0").slice(0, 2)}
+                </Div>
+                <Div
+                    style={{
+                        width: "100%",
+                        padding: 12,
+                        columnGap: 6,
+                    }}
+                >
+                    <Div
+                        style={{
+                            width: 168,
+                            height: 88,
+                            borderRadius: [44, 8, 8, 44],
+                            backgroundColor: "#48494B",
+                            fontColor: "white",
+                            fontSize: 30,
+                            textAlign: "center",
+                        }}
+                        onClick={() => {
+                            setStatus("init")
+                            setTime(0)
+                        }}
+                    >
+                        Cancel
+                    </Div>
+                    <Div
+                        style={{
+                            width: 168,
+                            height: 88,
+                            borderRadius: [8, 44, 44, 8],
+                            backgroundColor: "#FF9500",
+                            fontColor: "black",
+                            fontSize: 30,
+                            textAlign: "center",
+                        }}
+                        onClick={() => {
+                            if (status == "running") setStatus("pause")
+                            else setStatus("running")
+                        }}
+                    >
+                        {status == "running" ? "Pause" : "Start"}
+                    </Div>
+                </Div>
+            </Div>
+        </Root>
+    )
+}
+
+const meui = new MEUI(396, 484)
+ReactMEUI.render(<App />, meui)
